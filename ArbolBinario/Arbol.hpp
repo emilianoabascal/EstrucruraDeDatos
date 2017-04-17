aa50e580932e725eb8b97c60fab81fea18194036
emilianoabascal <emilianoabascal@me.com>
2017-04-17 13:10:50 -0500

    :3

--- ArbolBinario/Arbol.hpp
+++ ArbolBinario/Arbol.hpp
@@ -1,2 +1,72 @@
 #include <iostream>
+using namespace std;
 
+class NodoArbol{
+	private:
+		
+		NodoArbol *derecho;
+		NodoArbol* izquierdo;
+	public:
+		int dato;
+		NodoArbol();
+		int RegresaInfo();
+		friend class Arbol;
+};
+class Arbol{
+	public:
+		Arbol();
+		void crearArbol(NodoArbol *Apunt);
+		NodoArbol *RegresaRaiz();
+		void ImprimeIzquierda(NodoArbol *Apunt);
+		
+	private:
+		NodoArbol *raiz;
+		
+};
+
+NodoArbol::NodoArbol(){
+	derecho = NULL;
+	izquierdo = NULL;
+}
+
+int NodoArbol::RegresaInfo(){
+	return dato;
+}
+
+Arbol::Arbol(){
+	raiz = NULL;
+}
+
+
+
+void Arbol:: crearArbol(NodoArbol *Apunt){
+	char Resp;
+	Apunt = new NodoArbol;
+	cout << "ingrese la informacion a almacenar";
+	cin >> Apunt->dato;
+	cout << Apunt -> dato << " Tiene hijo izquierdo?(S/N): ";
+	cin >> Resp;
+	
+	if (Resp == 's'){
+		crearArbol(Apunt->izquierdo);
+		Apunt -> izquierdo = raiz;
+	}
+	cout << Apunt -> dato << " Tiene hijo derecho?(S/N): ";
+	cin >> Resp;
+	if (Resp == 's'){
+		crearArbol(Apunt -> derecho);
+		Apunt -> derecho = raiz;
+	}
+	raiz = Apunt;
+	cout << Apunt -> dato;
+}
+
+void Arbol::ImprimeIzquierda(NodoArbol *Apunt){
+	if(Apunt){
+		if (Apunt -> izquierdo) {
+			cout << Apunt -> izquierdo ->dato;
+			ImprimeIzquierda(Apunt -> izquierdo);
+		}
+		ImprimeIzquierda(Apunt->derecho);
+	}
+}
--- ArbolBinario/main.cpp
+++ ArbolBinario/main.cpp
@@ -1,2 +1,12 @@
 #include <iostream>
+#include "Arbol.hpp"
+using namespace std;
+int main(){
+	Arbol prueba;
+	NodoArbol *ap;
+	prueba.crearArbol(ap);
 
+	
+	//cout << ap->dato;
+	cout << ;
+}
